f23a62c29b34e9663627fa2f90efc4969015ff24
Woldan <woldancontact@gmail.com>
2026-05-06 06:19:14 +0300

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,6 +1,7 @@
 local Games = {
-  [9872472334] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blox Fruit
-  [1268927906] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Muscle%20Legends.lua", -- Muscle Legends
+  [9872472334] = "https://raw.githubusercontent.com/TheWoldan/WoldanHub/refs/heads/main/ScriptGames/Evade.lua", -- EVADE
+  [1268927906] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Muscle%20Legends.lua", -- ML
+  [893973440] = "",
 }
 
 return Games
