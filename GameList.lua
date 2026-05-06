fa134ce31c8d51bb3c3c48de86e6b68043bf5af8
Woldan <woldancontact@gmail.com>
2026-05-06 05:47:19 +0300

    Create GameList.lua

---
+++ GameList.lua
@@ -0,0 +1,6 @@
+local Games = {
+  [9872472334] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blox Fruit
+  [1268927906] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Muscle%20Legends.lua", -- Muscle Legends
+}
+
+return Games
