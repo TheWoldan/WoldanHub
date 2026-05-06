7eb8faf0f66d5c05e6d1339ba4bb6d27c9f7458b
Woldan <woldancontact@gmail.com>
2026-05-06 06:33:01 +0300

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,7 +1,7 @@
 local Games = {
   [9872472334] = "https://raw.githubusercontent.com/TheWoldan/WoldanHub/refs/heads/main/ScriptGames/Evade.lua", -- EVADE
   [1268927906] = "https://raw.githubusercontent.com/TheWoldan/WoldanHub/refs/heads/main/ScriptGames/MuscleLegends.lua", -- ML
-  [893973440] = "https://raw.githubusercontent.com/TheWoldan/WoldanHub/refs/heads/main/ScriptGames/FTF.lua", -- FTF
+  [893973440] = "https://raw.githubusercontent.com/TheWoldan/WoldanHub/refs/heads/main/ScriptGames/FTF.lua", -- ML
 }
 
 return Games
